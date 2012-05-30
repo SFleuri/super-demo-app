35249e8b2e011b98040f1972506f50f771a872a7
Tango <Tango.S.Fleuri@Gmail.com>
2012-05-30 23:30:06 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperDemoApp::Application.config.secret_token = '9dde7ccf8356ccce8af7c2a2c4c2714f517035e74a443d95186f0850021c53d3cdbc42b464b6dfdeed5d1980162e7e5e92290d9f06738784a0ecdf14906893b4'
